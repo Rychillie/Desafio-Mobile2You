221efefcbd498823f2b78e0d607189e82bb6a906
Rychillie <rychillie@hotmail.com>
2021-10-17 16:46:44 -0300

    organizando código header

--- Mobile2You/View/MovieView.swift
+++ Mobile2You/View/MovieView.swift
@@ -10,59 +10,69 @@ import SwiftUI
 struct MovieView: View {
     @ObservedObject var viewModel: MovieViewModel
     
-    internal let imageEffect: LinearGradient = LinearGradient(gradient: Gradient(colors: [Color.black,Color.clear]),
-                                                              startPoint: .center, endPoint: .bottom)
-    
     var body: some View {
         ZStack {
             Color.black.edgesIgnoringSafeArea(.all)
-            VStack{
-                Image(uiImage: "https://image.tmdb.org/t/p/original/\(viewModel.movie?.poster_path ?? "")".load())
-                    .resizable()
-                    .mask(self.imageEffect)
-                    .scaledToFit()
-                HStack {
-                    Text(viewModel.movie?.original_title ?? "").foregroundColor(.white)
-                        .font(.system(size: 24))
-                        .fontWeight(.black)
-                        .padding(.leading, 20)
-                    Spacer()
-                    Button {
-                        viewModel.isFavorite.toggle()
-                    } label: {
-                        Image(systemName: viewModel.imageName)
-                            .resizable()
-                            .frame(width: 20,height: 20)
-                            .foregroundColor(.white)
-                            .padding(.trailing, 20)
-                    }
-
-                }
-                HStack(alignment: .firstTextBaseline) {
-                    HStack{
-                        Image(systemName: "heart.fill").resizable()
-                            .frame(width: 15,height: 15)
-                            .foregroundColor(.white)
-                        Text(String(format: "%@ Likes",String(viewModel.movie?.vote_count ?? 0)))
-                            .foregroundColor(.white)
-                            .font(.system(size: 16))
-                    }
-                    HStack{
-                        Image(systemName: "flame").resizable()
-                            .frame(width: 15,height: 15)
-                            .foregroundColor(.white)
-                        Text(String(format: "%d Views",String(viewModel.movie?.popularity ?? 0)))
-                            .foregroundColor(.white)
-                            .font(.system(size: 16))
-                    }.padding(.leading,20)
-                    Spacer()
-                }.padding(.horizontal,20)
-            }.padding(.bottom,20)
+            MovieHeaderView().environmentObject(viewModel)
         }.onAppear {
             MovieAPI().getFirstMovie(id: viewModel.firstIdMovie) { movie in
                 viewModel.movie = movie
             }
         }
+    }
+}
+
+struct MovieHeaderView: View {
+    @EnvironmentObject var viewModel: MovieViewModel
+    
+    internal let imageEffect: LinearGradient = LinearGradient(
+        gradient: Gradient(colors: [Color.black,Color.clear]),
+        startPoint: .center, endPoint: .bottom
+    )
+    
+    var body: some View {
+        VStack{
+            Image(uiImage: "https://image.tmdb.org/t/p/original/\(viewModel.movie?.poster_path ?? "")".load())
+                .resizable()
+                .mask(self.imageEffect)
+                .scaledToFit()
+            HStack {
+                Text(viewModel.movie?.original_title ?? "").foregroundColor(.white)
+                    .font(.system(size: 24))
+                    .fontWeight(.black)
+                    .padding(.leading, 20)
+                Spacer()
+                Button {
+                    viewModel.isFavorite.toggle()
+                } label: {
+                    Image(systemName: viewModel.imageName)
+                        .resizable()
+                        .frame(width: 20,height: 20)
+                        .foregroundColor(.white)
+                        .padding(.trailing, 20)
+                }
+
+            }
+            HStack(alignment: .firstTextBaseline) {
+                HStack{
+                    Image(systemName: "heart.fill").resizable()
+                        .frame(width: 15,height: 15)
+                        .foregroundColor(.white)
+                    Text(String(format: "%@ Likes",String(viewModel.movie?.vote_count ?? 0)))
+                        .foregroundColor(.white)
+                        .font(.system(size: 16))
+                }
+                HStack{
+                    Image(systemName: "flame").resizable()
+                        .frame(width: 15,height: 15)
+                        .foregroundColor(.white)
+                    Text(String(format: "%d Views",String(viewModel.movie?.popularity ?? 0)))
+                        .foregroundColor(.white)
+                        .font(.system(size: 16))
+                }.padding(.leading,20)
+                Spacer()
+            }.padding(.horizontal,20)
+        }.padding(.bottom,20)
     }
 }
 
